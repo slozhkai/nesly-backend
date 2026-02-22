6636e87974a73a822e6bb3eefa559721f70975ce
slozhkai <sisipopakaka@gmail.com>
2026-02-23 01:21:08 +0300

    feat: refactoring

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/reponse-user.dto';
-import { UserMappers } from './user.mappers';
+import { userEntityToDto } from './user.mappers';
 
 @Controller('user')
 export class UserController {
@@ -10,15 +10,13 @@ export class UserController {
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-    return this.userService
-      .create(createUserDto)
-      .then((user) => UserMappers.userEntityToDto(user));
+    return this.userService.create(createUserDto).then(userEntityToDto);
   }
 
   @Get()
   async find(): Promise<UserResponseDto[]> {
     return this.userService
       .findAll()
-      .then((users) => users.map((user) => UserMappers.userEntityToDto(user)));
+      .then((users) => users.map(userEntityToDto));
   }
 }
--- src/user/user.mappers.ts
+++ src/user/user.mappers.ts
@@ -1,13 +1,11 @@
 import { UserEntity } from '../entities/user.entity';
 import { UserResponseDto } from './dto/reponse-user.dto';
 
-export class UserMappers {
-  static userEntityToDto(user: UserEntity): UserResponseDto {
-    return {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      gender: user.gender,
-    };
-  }
+export function userEntityToDto(user: UserEntity): UserResponseDto {
+  return {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    gender: user.gender,
+  };
 }
