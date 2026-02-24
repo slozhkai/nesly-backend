efb6aebe09b0e42803bf612ceb1bb538622e5392
slozhkai <sisipopakaka@gmail.com>
2026-02-24 23:26:41 +0300

    feat: add cookie parser to project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
